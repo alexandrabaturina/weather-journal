e9dbbea3449fc18900843bdba5bd4ed539ddc89d
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-05-16 10:52:33 -0700

    feat: retreive data from external API

--- website/app.js
+++ website/app.js
@@ -1,5 +1,27 @@
 /* Global Variables */
 
+//Base URL and API for Open Weather Map API call
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&APPID=dfd91166f2efa827b554cd87b63c1c8d';
+
+const zip = '98109';
+targetURL = baseURL + zip + apiKey;
+
+const retrieveData = async (url) => {
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+        const allData = await request.json()
+        console.log(allData);
+    }
+    catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
+
+retrieveData(targetURL);
+
 const postData = async (url = '', data = {}) => {
     console.log(data);
     const response = await fetch(url, {
@@ -21,13 +43,13 @@ const postData = async (url = '', data = {}) => {
     }
 }
 // Fake data to test addData function
-newData = {
-    temperature: 25,
-    date: '5/15/20',
-    userResponse: 'Feeling good'
-}
+// newData = {
+//     temperature: 25,
+//     date: '5/15/20',
+//     userResponse: 'Feeling good'
+// }
 
-postData('/addData', newData);
+// postData('/addData', newData);
 
 // // Create a new date instance dynamically with JS
 // let d = new Date();
