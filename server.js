b6be8ae55e2bbadc3f9d274868b6dfba84a61387
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-10-07 16:20:17 -0700

    refactor: delete unnecessary comments

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
 const express = require('express');
-
-// Start up an instance of app
 const app = express();
 
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
 
-// Initialize the main project folder
 app.use(express.static('website'));
 
-
-// Setup Server
 const port = 3030;
 const server = app.listen(port, listening);
 
 function listening() {
-    console.log(`server running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 }
 
 // Create GET route for url /all
