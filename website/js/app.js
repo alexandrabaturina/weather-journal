1474ddf63d3562e996283468c2189a72da02eb4c
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-10-08 11:59:21 -0700

    feat: handle zip errors

--- website/js/app.js
+++ website/js/app.js
@@ -5,15 +5,23 @@ let today = new Date().toDateString();
 
 document.getElementById('generate').addEventListener('click', generateData);
 
+const zipError = document.querySelector('.zip-error');
+
 
 function generateData(event) {
+    zipError.style.display === 'none';
+    zipError.innerText = '';
     const zipCode = document.getElementById('zip').value;
+    if (!zipCode) {
+        zipError.style.display === 'none';
+        zipError.innerText = "Please enter zip.";
+    }
     const targetURL = baseURL + zipCode + '&appid=' + apiKey + '&units=imperial';
     const content = document.getElementById('feelings').value;
     retrieveData(targetURL)
         .then(function (jsonData) {
-            postData('/addData', { date: today, temp: jsonData.main.temp, content: content });
-            return { date: today, temp: jsonData.main.temp, content: content };
+            postData('/addData', { date: today, temp: jsonData.main.temp, content: content, city: jsonData.name });
+            return { date: today, temp: jsonData.main.temp, content: content, city: jsonData.name };
         })
         .then(function () {
             updateUI()
@@ -26,7 +34,12 @@ const retrieveData = async (url) => {
     const request = await fetch(url);
     try {
         // Transform into JSON
-        const jsonData = await request.json()
+        const jsonData = await request.json();
+        if (jsonData.cod === "404") {
+            zipError.style.display = 'block';
+            zipError.innerText = "No data on server for this zip. Please enter another zip.";
+            return;
+        }
         return jsonData;
     }
     catch (error) {
@@ -59,14 +72,11 @@ const updateUI = async () => {
     try {
         const allData = await request.json();
         document.getElementById('entry-header').style.display = 'none';
-        document.getElementById('date').innerHTML = allData.date;
-        document.getElementById('temp').innerHTML = `${allData.temp} &deg; F`;
-        document.getElementById('content').innerHTML = allData.content;
+        document.getElementById('date').innerText = allData.date;
+        document.getElementById('temp').innerHTML = `${allData.temp} &deg;F in ${allData.city}`;
+        document.getElementById('content').innerText = allData.content;
         document.getElementById('zip').value = '';
         document.getElementById('feelings').value = '';
-
-
-
     } catch (error) {
         console.log("error", error);
     }
